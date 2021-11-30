7edbbe14d3b94955f1054e65473828c14d28937a
fuzzext <goncalomachadoxt@gmail.com>
2021-11-30 10:49:45 -0100

    trabalhohome

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+ 
+app.use(express.static('./public'))
+
+app.use(express.static('./public'))
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+
+})
+
+app.get('/:nome', function (req, res) {
+    res.send('Hello '+ req.params.nome)
+  })
+const port = 3000
+ 
+app.listen(port, () => {
+    console.log(`Listenning  on port ${port}`)
+})
